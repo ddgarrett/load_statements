aee42e2f21881021f8073bba5390b42df483fb32
ddgarrett <doug@anetsc.com>
2023-12-21 17:37:01 -0800

    Initial Main

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+    Main program for Load Statements.
+
+    Read a CSV line by line, splitting it by commas.
+
+    Ignore any blank lines
+    If line[0] == 'Account Type' process account summary.
+    Else if line[0] == 'Symbol/CUSIP' process account details.
+    Else ignore the line.
+
+'''
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
