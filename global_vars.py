4ac4b97ff1e2474507008de46f7c28144bb56dd1
ddgarrett <doug@anetsc.com>
2023-12-22 16:01:55 -0800

    Create Globals .py

---
+++ global_vars.py
@@ -0,0 +1,17 @@
+'''
+    Global fields including configuration values
+'''
+
+from __future__ import annotations
+
+from table import Table
+
+accounts:Table = None   # Table defining accounts
+stocks:Table   = None   # Table defining stocks within an account
+
+txn_col = ["Date","Description","Amount","Account Name","Labels"]
+transactions   = Table.new_table(txn_col)
+
+reader = None  # CSV Reader
+
+func_lookup:dict[list[str],callable] = None
