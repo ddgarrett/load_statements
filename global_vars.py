07e37673ce79475e0be280e284a5750f577ebc1e
ddgarrett <doug@anetsc.com>
2024-02-14 08:28:35 -0800

    Add Description of Function Lookup Dictionary

--- global_vars.py
+++ global_vars.py
@@ -11,4 +11,6 @@ stocks:Table   = None   # Table defining stocks within an account
 
 reader = None  # CSV Reader
 
+# lookup function to call 
+# based on CSV header
 func_lookup:dict[list[str],callable] = {}
